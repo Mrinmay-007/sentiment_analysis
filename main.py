9949506c8e965859f8a6d557edd2fcb6f63f8df3
mrinmay2004 <mrinmay21012204@gmail.com>
2026-03-24 16:52:44 +0530

    Updated

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from routers import predict
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(title="Sentiment Analysis Prediction API")
+
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # for development
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(predict.router)
 
